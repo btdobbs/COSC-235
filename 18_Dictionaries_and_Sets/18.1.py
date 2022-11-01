690bc4deaea55418804f19c0aed5f5cc8ac2a9e6
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-01 16:54:04 -0400

    Update 18.1.py

--- 18_Dictionaries_and_Sets/18.1.py
+++ 18_Dictionaries_and_Sets/18.1.py
@@ -1,1 +1,11 @@
+def main():
+    color_names = {
+        'red': tuple([255, 0, 0]),
+        'green': tuple([0, 255, 0]),
+        'blue': tuple([0, 0, 255]),
+        'yellow': tuple([255, 255, 0])
+    }
+    print(color_names)
 
+
+main()
