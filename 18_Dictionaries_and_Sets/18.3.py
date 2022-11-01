165e67b49a2b6114f46b38e6a6baa018da6b7450
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-01 16:54:46 -0400

    Update 18.3.py

--- 18_Dictionaries_and_Sets/18.3.py
+++ 18_Dictionaries_and_Sets/18.3.py
@@ -1,1 +1,17 @@
+def word_count(words):
+    word_count_dictionary = {}
+    for word in words:
+        if word in word_count_dictionary:
+            word_count_dictionary[word] = word_count_dictionary[word] + 1
+        else:
+            word_count_dictionary[word] = 1
+    return word_count_dictionary
 
+
+def main():
+    words = ['Paris', 'in', 'the', 'the', 'spring']
+    word_count_dictionary = word_count(words)
+    print(word_count_dictionary)
+
+
+main()
