fb81e34760b9731f193430e2fd2df39238a027e9
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-01 16:54:25 -0400

    Update 18.2.py

--- 18_Dictionaries_and_Sets/18.2.py
+++ 18_Dictionaries_and_Sets/18.2.py
@@ -1,1 +1,14 @@
+def unique_items(lst):
+    item_set = set()
+    for item in lst:
+        item_set.add(item)
+    return len(lst) == len(item_set)
+
+
+def main():
+    print(unique_items([1, 2, 3, 4]))
+    print(unique_items([1, 2, 2, 3, 4]))
+
+
+main()
 
