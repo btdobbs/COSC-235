7162037d95e227135bc3ed94f6505d26b13ffbf6
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-01 16:55:28 -0400

    Update 18.4.py

--- 18_Dictionaries_and_Sets/18.4.py
+++ 18_Dictionaries_and_Sets/18.4.py
@@ -1,1 +1,16 @@
+def find_bad_words(words, good_words):
+    bad_words = set()
+    for word in words:
+        if word not in good_words:
+            bad_words.add(word)
+    return bad_words
 
+
+def main():
+    words = ['apple', 'yoyo', 'peach', 'top']
+    good_words = ['apple', 'cherry', 'peach', 'plum']
+    bad_words = find_bad_words(words, good_words)
+    print(bad_words)
+
+
+main()
