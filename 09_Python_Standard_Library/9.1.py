20f5f978d16c00585e8140f0555935dd6e35eedd
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 16:51:47 -0400

    Update 9.1.py

--- 09_Python_Standard_Library/9.1.py
+++ 09_Python_Standard_Library/9.1.py
@@ -1,1 +1,12 @@
+import math
 
+# Test with 0, 45, 90, 180, and 225
+theta = float(input("Enter \u03B8 in degrees: "))
+radians = math.radians(theta)
+sine = math.sin(theta)
+cosine = math.cos(theta)
+pythagorean_identity = sine*sine+cosine*cosine
+print("\u03B8 in radians:", radians)
+print("sin(\u03B8):", sine)
+print("cos(\u03B8):", cosine)
+print("Pythagorean Identity:", round(pythagorean_identity))
