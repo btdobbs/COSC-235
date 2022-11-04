c6dd99988a442b9f3b532fd9dce6f94636b0590f
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 16:56:51 -0400

    Create 9.2.py

---
+++ 09_Python_Standard_Library/9.2.py
@@ -0,0 +1,14 @@
+import random
+
+wb1 = random.randint(1, 70)
+wb2 = random.randint(1, 70)
+wb3 = random.randint(1, 70)
+wb4 = random.randint(1, 70)
+wb5 = random.randint(1, 70)
+yb = random.randint(1, 25)
+print(wb1)
+print(wb2)
+print(wb3)
+print(wb4)
+print(wb5)
+print(yb)
