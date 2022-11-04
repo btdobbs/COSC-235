1f94a072a6bf0e5f5169c27dfbec2442dd01de46
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 19:07:54 -0400

    Update 14.1.py

--- 14_Conditional_Statements/14.1.py
+++ 14_Conditional_Statements/14.1.py
@@ -1,1 +1,40 @@
+def max_of_3(a, b, c):
+    max_value = None
+    if a > b > c:
+        max_value = a
+    if a > c > b:
+        max_value = a
+    if b > a > c:
+        max_value = b
+    if b > c > a:
+        max_value = b
+    if c > a > b:
+        max_value = c
+    if c > b > a:
+        max_value = c
+    return max_value
 
+
+def max_of_3_with_equals(a, b, c):
+    max_value = None
+    if a >= b >= c:
+        max_value = a
+    if a >= c >= b:
+        max_value = a
+    if b >= a >= c:
+        max_value = b
+    if b >= c >= a:
+        max_value = b
+    if c >= a >= b:
+        max_value = c
+    if c >= b >= a:
+        max_value = c
+    return max_value
+
+
+def main():
+    print(max_of_3(3, 3, 5))  # prints None
+    print(max_of_3_with_equals(3, 3, 5))  # prints 5
+
+
+main()
