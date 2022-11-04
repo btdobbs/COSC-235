76f81f65b70bd72a6df018d00683262d43be29a8
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 18:56:12 -0400

    Update 13.4.py

--- 13_Creating_Functions/13.4.py
+++ 13_Creating_Functions/13.4.py
@@ -1,1 +1,11 @@
+def bmi(height, weight):
+    return (703 * weight)/(height*height)
 
+
+def main():
+    person_height = float(input('Enter your height in inches: '))
+    person_weight = float(input('Enter your weight in pounds: '))
+    print(f'Your BMI is {bmi(person_height, person_weight)}')
+
+
+main()
