3d94afdd30f8aef7956701a66faf466900d818e9
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 17:59:56 -0400

    Update 11.2.py

--- 11_Lists/11.2.py
+++ 11_Lists/11.2.py
@@ -1,1 +1,13 @@
+import statistics
 
+input_line = input('Enter quiz scores separated by spaces: ')
+input_line = input_line.replace(",", " ")
+scores = [int(s) for s in input_line.split()]
+average_score = statistics.mean(scores)
+standard_deviation = statistics.stdev(scores)
+median = statistics.median(scores)
+mode = statistics.mode(scores)
+print('The average score is', average_score, end='.\n')
+print('The standard deviation of scores is', standard_deviation, end='.\n')
+print('The median of scores is', median, end='.\n')
+print('The mode of scores is', mode, end='.\n')
