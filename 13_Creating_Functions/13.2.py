243fdd3ff90149ee9015ab90e1bbc02c9e2a71b1
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 18:44:41 -0400

    Update 13.2.py

--- 13_Creating_Functions/13.2.py
+++ 13_Creating_Functions/13.2.py
@@ -1,1 +1,16 @@
+import random
 
+
+def rock_paper_scissors():
+    lst = ['rock', 'paper', 'scissors']
+    return random.choice(lst)
+
+
+def main():
+    print(rock_paper_scissors())
+    print(rock_paper_scissors())
+    print(rock_paper_scissors())
+    print(rock_paper_scissors())
+
+
+main()
