19ff54f67914cc60f34426e3d088de92335617ab
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 16:28:00 -0400

    Update 7.1.py

--- 07_Input_and_Output/7.1.py
+++ 07_Input_and_Output/7.1.py
@@ -1,1 +1,11 @@
+import math
 
+diameter = float(input("Enter diameter: "))
+length = float(input("Enter length: "))
+X = math.pi * (diameter / 2.0) ** 2
+C = math.pi * diameter
+A = 2 * X + C * length
+V = X * length
+print("circumference is", C)
+print("surface area is", A)
+print("volume is", V)
