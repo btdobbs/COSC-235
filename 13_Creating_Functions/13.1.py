025932436080069f5f07b72778e204047ed86fd4
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 18:41:34 -0400

    Update 13.1.py

--- 13_Creating_Functions/13.1.py
+++ 13_Creating_Functions/13.1.py
@@ -1,1 +1,12 @@
+def to_fahrenheit(temperature):
+    return round(((9/5) * temperature) + 32, 1)
 
+
+def main():
+    print(to_fahrenheit(0), "°F")  # prints 32 °F
+    print(to_fahrenheit(24), "°F")  # prints 75.2 °F
+    print(to_fahrenheit(37), "°F")  # prints 98.6 °F
+    print(to_fahrenheit(100), "°F")  # prints 212 °F
+
+
+main()
