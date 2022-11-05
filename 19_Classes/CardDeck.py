0afba76b2402cafe14fd8c2fc7cc27acb53fb50a
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 18:00:32 -0400

    Update CardDeck.py

--- 19_Classes/CardDeck.py
+++ 19_Classes/CardDeck.py
@@ -1,1 +1,19 @@
+import random
+import Card
 
+
+class CardDeck:
+    def __init__(self):
+        self._cards = []
+        for suit in Card.SUITS:
+            for rank in Card.RANKS:
+                self._cards.append(Card.Card(rank, suit, Card.FACE_DOWN))
+
+    def shuffle(self):
+        random.shuffle(self._cards)
+
+    def draw(self):
+        return self._cards.pop()
+
+    def is_empty(self):
+        return len(self._cards) == 0
