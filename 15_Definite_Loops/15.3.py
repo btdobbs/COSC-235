9f090b486872b11766189e17d87db0f7a18b61cc
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 20:35:18 -0400

    Update 15.3.py

--- 15_Definite_Loops/15.3.py
+++ 15_Definite_Loops/15.3.py
@@ -1,1 +1,16 @@
+import math
 
+
+def main():
+    n = int(input("Enter a number? "))
+    if n <= 1:
+        print(n, "is not prime")
+        return
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            print(n, "is not prime")
+            return
+    print(n, "is prime")
+
+
+main()
