670ffb1bee6fb9a4a654469708e9bcf44443d9c1
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 20:48:30 -0400

    Update 15.5.py

--- 15_Definite_Loops/15.5.py
+++ 15_Definite_Loops/15.5.py
@@ -1,1 +1,18 @@
+import math
 
+
+def index_smallest_positive(lst):
+    current_min = math.inf
+    current_min_index = -1
+    for i, n in enumerate(lst):
+        if 0 < n < current_min:
+            current_min_index = i
+            current_min = n
+    return current_min_index
+
+
+def main():
+    print(index_smallest_positive([-5, 0, 5, 4, 100, 1, 100, -10, 50, 1]))
+
+
+main()
