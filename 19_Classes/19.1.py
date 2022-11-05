ce3f03a98ad56d2c9d4fdec269f56b717c0bb14a
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 12:14:26 -0400

    Update 19.1.py

--- 19_Classes/19.1.py
+++ 19_Classes/19.1.py
@@ -1,1 +1,57 @@
+import random
+
+ACE = 1
+JACK = 11
+QUEEN = 12
+KING = 13
+
+CLUBS = 'Clubs'
+DIAMONDS = 'Diamonds'
+HEARTS = 'Hearts'
+SPADES = 'Spades'
+
+FACE_UP = True
+FACE_DOWN = False
+
+RANKS = (ACE, 2, 3, 4, 5, 6, 7, 8, 9, 10, JACK, QUEEN, KING)
+SUITS = (CLUBS, DIAMONDS, HEARTS, SPADES)
+
+
+class Card:
+    def __init__(self, rank, suit, is_face_up):
+        self._rank = rank
+        self._suit = suit
+        self._is_face_up = is_face_up
+
+    def flip(self):
+        self._is_face_up = not self._is_face_up
+
+    def is_face_up(self):
+        return self._is_face_up
+
+    def get_rank(self):
+        return self._rank
+
+    def get_suit(self):
+        return self._suit
+
+    def __str__(self):
+        if not self.is_face_up():
+            return '###'
+        rank_names = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', ' Nine', 'Ten', ' Jack', ' Queen',
+                      ' King')
+        return f'{rank_names[self.get_rank() - 1]} of {self.get_suit()}'
+
+
+def main():
+    rank = random.choice(RANKS)
+    suit = random.choice(SUITS)
+    status = random.choice((False, True))
+    card = Card(rank, suit, status)
+    if not card.is_face_up():
+        card.flip()
+    print('The card selected is', card)
+
+
+main()
 
