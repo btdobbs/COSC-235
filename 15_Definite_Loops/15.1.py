86c92cfbebcfd6fba27fac1fb82def7e1f3800bc
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-04 20:23:40 -0400

    Update 15.1.py

--- 15_Definite_Loops/15.1.py
+++ 15_Definite_Loops/15.1.py
@@ -1,1 +1,9 @@
+def main():
+    hour_sum = 0
+    for day in range(7):
+        prompt = "Enter hours worked on day " + str(day + 1) + ": "
+        hour_sum += int(input(prompt))
+    print(hour_sum, "total hour(s) worked")
 
+
+main()
