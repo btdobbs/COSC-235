26ef30285a8d30e866cc53793f0f4bf1fb250cc5
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 11:16:34 -0400

    Create 16.3.py

---
+++ 16_Indefinite_Loops/16.3.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input('Enter a positive integer: '))
+    inputs = []
+    index = 0
+    while index < n:
+        inputs.append(int(input('Enter a number: ')))
+        index += 1
+
+
+main()
