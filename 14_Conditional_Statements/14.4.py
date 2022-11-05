e0a5158b8f886d2a82914dd270b6dfcf9ab28c0d
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 11:58:50 -0400

    Update 14.4.py

--- 14_Conditional_Statements/14.4.py
+++ 14_Conditional_Statements/14.4.py
@@ -1,28 +1,28 @@
-rock = 'rock'
-paper = 'paper'
-scissors = 'scissors'
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSORS = 'scissors'
 
 
 def decide(choice1, choice2):
     if choice1 == choice2:
         return 0
-    if (choice1 == paper and choice2 == rock) or (choice1 == rock and choice2 == scissors) or (
-            choice1 == scissors and choice2 == paper):
+    if (choice1 == PAPER and choice2 == ROCK) or (choice1 == ROCK and choice2 == SCISSORS) or (
+            choice1 == SCISSORS and choice2 == PAPER):
         return 1
     else:
         return 2
 
 
 def main():
-    print(decide(rock, rock))
-    print(decide(rock, paper))
-    print(decide(rock, scissors))
-    print(decide(paper, paper))
-    print(decide(paper, scissors))
-    print(decide(paper, rock))
-    print(decide(scissors, scissors))
-    print(decide(scissors, rock))
-    print(decide(scissors, paper))
+    print(decide(ROCK, ROCK))
+    print(decide(ROCK, PAPER))
+    print(decide(ROCK, SCISSORS))
+    print(decide(PAPER, PAPER))
+    print(decide(PAPER, SCISSORS))
+    print(decide(PAPER, ROCK))
+    print(decide(SCISSORS, SCISSORS))
+    print(decide(SCISSORS, ROCK))
+    print(decide(SCISSORS, PAPER))
 
 
 main()
