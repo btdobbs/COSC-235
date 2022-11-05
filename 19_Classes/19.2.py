3cf82ffdee2a43f117db5a1d4995b1db6ce9e4ce
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 12:45:56 -0400

    Update 19.2.py

--- 19_Classes/19.2.py
+++ 19_Classes/19.2.py
@@ -1,1 +1,27 @@
+import random
+
+
+class Hopper:
+    def __init__(self, values):
+        self._hopper = list(values)
+
+    def draw(self):
+        b = random.choice(self._hopper)
+        self._hopper.remove(b)
+        return b
+
+
+def main():
+    white_hopper = Hopper(range(1, 70))
+    red_hopper = Hopper(range(1, 27))
+    wb1 = white_hopper.draw()
+    wb2 = white_hopper.draw()
+    wb3 = white_hopper.draw()
+    wb4 = white_hopper.draw()
+    wb5 = white_hopper.draw()
+    rb = red_hopper.draw()
+    print('Picks:', wb1, wb2, wb3, wb4, wb5, rb)
+
+
+main()
 
