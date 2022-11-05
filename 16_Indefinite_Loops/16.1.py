294b779c58cb9eb2127e4bfccf3b76e8662ca496
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-05 11:10:34 -0400

    Update 16.1.py

--- 16_Indefinite_Loops/16.1.py
+++ 16_Indefinite_Loops/16.1.py
@@ -1,1 +1,12 @@
+def main():
+    count = 0
+    n = 0
+    while n != "":
+        n = input("Enter a number: ")
+        if n != "" and int(n) % 2 == 1:
+            count += 1
 
+    print(count,"odd numbers were entered")
+
+
+main()
